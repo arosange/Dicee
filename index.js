eb2f07046e444635d77109bfb673a266ec4ec142
arosange <58056208+arosange@users.noreply.github.com>
2024-10-16 12:00:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-  let  randomImage1= `./dice${randomNumber1}.png`;
+  let  randomImage1= `dice${randomNumber1}.png`;
   let  image1=document.querySelectorAll("img")[0];
 
   image1.setAttribute("src",randomImage1);
 
   let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-  let  randomImage2= `./dice${randomNumber2}.png`;
+  let  randomImage2= `dice${randomNumber2}.png`;
   let  image2=document.querySelectorAll("img")[1];
 
   image2.setAttribute("src",randomImage2);
